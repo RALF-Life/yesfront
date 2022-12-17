3dfa57ba42354a0ef8d7d96afe2c85c46168773e
Luca Hackl <lucamiguelhackl@gmail.com>
2022-12-17 01:29:33 +0100

    Created imprint

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens:{
+      'sm': {'min': '0px', 'max': '700px'},
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors:{
+        website:{
+          50: '#8E8E8E'
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-font-inter')],
 }
