ddbefe95526893fb66f7e8051210ecacd8092f73
Luca Hackl <lucamiguelhackl@gmail.com>
2023-05-20 20:44:20 +0200

    Fixed scrollbars and added some more UI

---
+++ pages/flows.jsx
@@ -0,0 +1,92 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React from 'react';
+import Editor, {DiffEditor, useMonaco, loader} from '@monaco-editor/react';
+
+
+export default function Flows() {
+    const [input, setInput] = React.useState("//Your YAML Code");
+
+    const editorRef = React.useRef(null);
+
+    function handleEditorDidMount(editor, monaco) {
+        editorRef.current = editor;
+    }
+
+    React.useEffect(() => {
+        if (editorRef.current) {
+            const state = editorRef.current.saveViewState();
+            editorRef.current.setValue(input);
+            editorRef.current.restoreViewState(state);
+        }
+    }, [input]);
+
+    return (
+        <div className="h-screen">
+            <Head>
+                <title>Ralf</title>
+                <meta name="description" content="RALF Flow creation"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div id="top-navigation" className="ml-5 mt-3 flex items-center">
+                <p id="logo"
+                   className="mr-6 font-black bg-cover text-4xl text-transparent bg-clip-text bg-gradient-to-r from-RALF-gradient-start to-RALF-gradient-end">RALF</p>
+                <div className="flex space-x-6 items-center">
+                    <Link href="/imprint" className="text-footer-color font-semibold">Imprint</Link>
+                    <Link href="https://ralf-p.medium.com/" className="text-footer-color font-semibold">Blog</Link>
+                </div>
+                <div className="ml-auto flex items-center mr-10">
+                    <Link href="/signup" className="bg-white rounded-lg px-5 py-1 font-semibold text-center">Sign
+                        Up</Link>
+                </div>
+            </div>
+
+            <div className="w-screen">
+                <div className="h-full flex justify-between items-center text-white pt-20">
+                    <div className="w-1/2 ml-8">
+                        <div className="flex justify-between items-center mb-2">
+                            <p className="text-white font-bold mb-8 text-4xl">Test Flow</p>
+                            <button className=" text-black font-semibold px-4 py-2 rounded-lg bg-[#50FFD5] h-10">Save
+                            </button>
+                        </div>
+                        <Editor
+                            height="80vh"
+                            theme="vs-dark"
+                            defaultValue={input}
+                            onChange={setInput}
+                            onMount={handleEditorDidMount}
+                            saveViewState
+                        />
+                    </div>
+
+                    <div id="inside-catalogue"
+                         className="w-2/5 h-1/2 m-auto bg-[#0B0B0B] flex flex-col justify-start items-center border-[#404040] rounded-lg border-2 p-6">
+                        <div className="relative w-full flex justify-start mt-3">
+                            <div
+                                className="absolute top-1/2 w-full border-t-2 border-dotted border-white opacity-70"></div>
+                            <p className="font-bold text-white z-10 bg-[#0B0B0B] px-2">Action</p>
+                        </div>
+                        <div className="flex justify-around w-full mt-3">
+                            <div className="w-1/2 mb-3 h-20 rounded-lg border-2 m-3 mt-3"
+                                 onClick={() => setInput(prevInput => prevInput + "\n" + "if: '$1' \nthen: \n  - do: filters/filter-out")}>
+                                <p className="font-extrabold ml-3 mt-3">FILTER OUT</p>
+                                <p className="ml-3">Filters an Event out</p>
+                            </div>
+                            <div className="w-1/2 h-20 rounded-lg border-2 m-3 mt-3 mb-3"
+                                 onClick={() => setInput(prevInput => prevInput + "\n" + "test")}>
+                                <p className="font-extrabold ml-3 mt-3">FILTER IN</p>
+                                <p className="ml-3">Filters an Event in</p>
+                            </div>
+                        </div>
+                        <div className="w-full h-20 rounded-lg border-2 mt-3 mb-3"
+                             onClick={() => setInput(prevInput => prevInput + "\n" + "test")}>
+                            <p className="font-extrabold ml-3 mt-3">REGEX REPLACE</p>
+                            <p className="ml-3">Replaces Text in an Event</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 
 export default function Signup() {
     return (
-        <div className="bg-black h-screen">
+        <div className="bg-black h-screen flex justify-center items-center">
             <Head>
                 <title>Ralf</title>
                 <meta name="description" content="RALF Landing page"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <div id="top-navigation" className="ml-5 mt-3 flex items-center">
+            <div id="top-navigation" className="ml-5 mt-3 flex items-center absolute top-0 left-0 right-0">
                 <p id="logo"
                    className="mr-6 font-black bg-cover text-4xl text-transparent bg-clip-text bg-gradient-to-r from-RALF-gradient-start to-RALF-gradient-end">RALF</p>
                 <div className="flex space-x-6 items-center">
@@ -21,35 +21,34 @@ export default function Signup() {
                 </div>
             </div>
 
-            <div className="h-screen flex justify-center items-center" id="login-center-box">
-                <div
-                    className="w-auto m-auto bg-[#0B0B0B] flex flex-col justify-center items-center border-[#404040] rounded-lg border-2 p-6">
-                    <div id="welcome-text" className="text-center mb-6">
-                        <p className="font-black bg-cover text-4xl text-transparent bg-clip-text bg-gradient-to-r from-RALF-gradient-start to-RALF-gradient-end">RALF</p>
-                        <p className="text-white font-bold">Create your account! 😊</p>
-                    </div>
-                    <div className="">
-                        <form action="/api/form" method="post" className="flex flex-col items-center">
-                            <input type="email" id="email" placeholder="    Email" name="email"
-                                   className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
+            <div
+                className="w-auto m-auto bg-[#0B0B0B] flex flex-col justify-center items-center border-[#404040] rounded-lg border-2 p-6"
+                id="login-center-box">
+                <div id="welcome-text" className="text-center mb-6">
+                    <p className="font-black bg-cover text-4xl text-transparent bg-clip-text bg-gradient-to-r from-RALF-gradient-start to-RALF-gradient-end">RALF</p>
+                    <p className="text-white font-bold">Create your account! 😊</p>
+                </div>
+                <div className="">
+                    <form action="/api/form" method="post" className="flex flex-col items-center">
+                        <input type="email" id="email" placeholder="    Email" name="email"
+                               className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
+                               required/>
+                        <input type="text" id="username" placeholder="    Username" name="username"
+                               className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
+                               required/>
+                        <input type="password" id="password" placeholder="    Password" name="password"
+                               className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
+                               required/>
+                        <div className="flex w-auto">
+                            <input type="password" id="password2" placeholder="   Repeat Password" name="password2"
+                                   className="text-footer-color rounded-lg mr-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
                                    required/>
-                            <input type="text" id="username" placeholder="    Username" name="username"
-                                   className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
-                                   required/>
-                            <input type="password" id="password" placeholder="    Password" name="password"
-                                   className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
-                                   required/>
-                            <div className="flex w-auto">
-                                <input type="password" id="password" placeholder="   Repeat Password" name="password"
-                                       className="text-footer-color rounded-lg mr-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
-                                       required/>
-                                <button type="submit"
-                                        className=" text-black font-semibold px-8 py-2 rounded-lg bg-[#50FFD5] h-10">
-                                    Register
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                            <button type="submit"
+                                    className=" text-black font-semibold px-8 py-2 rounded-lg bg-[#50FFD5] h-10">
+                                Register
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
