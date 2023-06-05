d315fdb5afbcab4cc2a61ed3c0798fad4de49d00
Luca Hackl <luca.hackl@sap.com>
2023-06-05 11:21:24 +0200

    Firebase 🔥

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -1,14 +1,31 @@
-import Head from 'next/head'
-import Link from 'next/link'
+'use client'
+import React from "react";
+import signIn from "../firebase/auth/signin";
+import { useRouter } from 'next/navigation'
+import Link from "next/link";
 
-export default function Signup() {
+function Page() {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const router = useRouter()
+
+    const handleForm = async (event) => {
+        event.preventDefault()
+
+        const { result, error } = await signIn(email, password);
+
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+        return router.push("/flows")
+    }
     return (
+
         <div className="bg-black h-screen flex justify-center items-center">
-            <Head>
-                <title>Ralf</title>
-                <meta name="description" content="RALF Landing page"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+
             <div id="top-navigation" className="ml-5 mt-3 flex items-center absolute top-0 left-0 right-0">
                 <p id="logo"
                    className="mr-6 font-black bg-cover text-4xl text-transparent bg-clip-text bg-gradient-to-r from-RALF-gradient-start to-RALF-gradient-end">RALF</p>
@@ -17,7 +34,8 @@ export default function Signup() {
                     <Link href="https://ralf-p.medium.com/" className="text-footer-color font-semibold">Blog</Link>
                 </div>
                 <div className="ml-auto flex items-center mr-10">
-                    <Link href="/" className="bg-white rounded-lg px-5 py-1 font-semibold text-center">Login</Link>
+                    <Link href="/signup" className="bg-white rounded-lg px-5 py-1 font-semibold text-center">Sign
+                        Up</Link>
                 </div>
             </div>
 
@@ -26,31 +44,27 @@ export default function Signup() {
                 id="login-center-box">
                 <div id="welcome-text" className="text-center mb-6">
                     <p className="font-black bg-cover text-4xl text-transparent bg-clip-text bg-gradient-to-r from-RALF-gradient-start to-RALF-gradient-end">RALF</p>
-                    <p className="text-white font-bold">Create your account! 😊</p>
+                    <p className="text-white font-bold">Welcome back! 😊</p>
                 </div>
                 <div className="">
-                    <form action="/api/form" method="post" className="flex flex-col items-center">
-                        <input type="email" id="email" placeholder="    Email" name="email"
+                    <form onSubmit={handleForm} className="flex flex-col items-center">
+                        <input
+                               onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com"
                                className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
-                               required/>
-                        <input type="text" id="username" placeholder="    Username" name="username"
-                               className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
-                               required/>
-                        <input type="password" id="password" placeholder="    Password" name="password"
-                               className="text-footer-color rounded-lg mb-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
-                               required/>
+                               />
                         <div className="flex w-auto">
-                            <input type="password" id="password2" placeholder="   Repeat Password" name="password2"
+                            <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password"
                                    className="text-footer-color rounded-lg mr-2 w-full h-10 bg-[#0B0B0B] border-2 border-[#404040]"
-                                   required/>
+                                   />
                             <button type="submit"
-                                    className=" text-black font-semibold px-8 py-2 rounded-lg bg-[#50FFD5] h-10">
-                                Register
+                                    className=" text-black font-semibold px-4 py-2 rounded-lg bg-[#50FFD5] h-10">Login
                             </button>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
-    )
+    </div>
+    );
 }
+
+export default Page;
