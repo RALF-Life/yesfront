d36cc73af39a55e0199ac3e020d2dba223178b5a
darmiel <71837281+darmiel@users.noreply.github.com>
2023-10-04 10:23:08 +0200

    fix: updated `et` URL

--- components/Var.js
+++ components/Var.js
@@ -1,5 +1,1 @@
-const config = {
-    ETBaseURL: process.env.ET_BASE_URL
-}
-// for some reason, loading from ENV doesn't work.
-export const ETBaseURL = 'https://et.ralf.life'
+export const ETBaseURL = process.env.NEXT_PUBLIC_ET_BASE_URL;
